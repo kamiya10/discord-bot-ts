d107290c489d13b04331d3ff75e0f7decc5d8fd4
Kamiya <58339640+kamiya10@users.noreply.github.com>
2024-10-11 10:50:50 +0800

    refactor: Awaitable

--- src/class/command.ts
+++ src/class/command.ts
@@ -1,17 +1,24 @@
-import type { AnySelectMenuInteraction, ApplicationCommandOptionChoiceData, AutocompleteInteraction, ButtonInteraction, ChatInputCommandInteraction, ModalSubmitInteraction } from 'discord.js';
+import type { AnySelectMenuInteraction, ApplicationCommandOptionChoiceData, AutocompleteInteraction, Awaitable, ButtonInteraction, ChatInputCommandInteraction, ModalSubmitInteraction } from 'discord.js';
 import type { ModalBuilder, SharedSlashCommand } from '@discordjs/builders';
 import type { ExtendedClient } from '@/class/client';
 
-export interface CommandOptions {
+interface CommandHandlers {
+  execute: (this: ExtendedClient, interaction: ChatInputCommandInteraction<'cached'>) => Awaitable<void>;
+  onAutocomplete?: (this: ExtendedClient, interaction: AutocompleteInteraction<'cached'>) => Awaitable<readonly ApplicationCommandOptionChoiceData[]>;
+  onButton?: (this: ExtendedClient, interaction: ButtonInteraction<'cached'>, buttonId: string) => Awaitable<void>;
+  onModalSubmit?: (this: ExtendedClient, interaction: ModalSubmitInteraction<'cached'>, modalId: string) => Awaitable<void>;
+  onSelectMenu?: (this: ExtendedClient, interaction: AnySelectMenuInteraction<'cached'>, menuId: string) => Awaitable<void>;
+}
+
+export interface CommandOptions extends CommandHandlers {
   builder: SharedSlashCommand;
   defer: boolean;
   ephemeral: boolean;
   modals?: Record<string, ModalBuilder>;
-  execute: (this: ExtendedClient, interaction: ChatInputCommandInteraction<'cached'>) => void | Promise<void>;
-  onAutocomplete?: (this: ExtendedClient, interaction: AutocompleteInteraction<'cached'>) => readonly ApplicationCommandOptionChoiceData[] | Promise<readonly ApplicationCommandOptionChoiceData[]>;
-  onButton?: (this: ExtendedClient, interaction: ButtonInteraction<'cached'>, buttonId: string) => void | Promise<void>;
-  onModalSubmit?: (this: ExtendedClient, interaction: ModalSubmitInteraction<'cached'>, modalId: string) => void | Promise<void>;
-  onSelectMenu?: (this: ExtendedClient, interaction: AnySelectMenuInteraction<'cached'>, menuId: string) => void | Promise<void>;
+}
+
+export interface SubCommand extends CommandHandlers {
+  builder: SharedSlashCommand;
 }
 
 export class Command {
@@ -19,11 +26,11 @@ export class Command {
   defer: boolean;
   ephemeral: boolean;
   modals?: Record<string, ModalBuilder>;
-  execute: (this: ExtendedClient, interaction: ChatInputCommandInteraction<'cached'>) => void | Promise<void>;
-  onAutocomplete?: (this: ExtendedClient, interaction: AutocompleteInteraction<'cached'>) => readonly ApplicationCommandOptionChoiceData[] | Promise<readonly ApplicationCommandOptionChoiceData[]>;
-  onButton?: (this: ExtendedClient, interaction: ButtonInteraction<'cached'>, buttonId: string) => void | Promise<void>;
-  onModalSubmit?: (this: ExtendedClient, interaction: ModalSubmitInteraction<'cached'>, modalId: string) => void | Promise<void>;
-  onSelectMenu?: (this: ExtendedClient, interaction: AnySelectMenuInteraction<'cached'>, menuId: string) => void | Promise<void>;
+  execute: (this: ExtendedClient, interaction: ChatInputCommandInteraction<'cached'>) => Awaitable<void>;
+  onAutocomplete?: (this: ExtendedClient, interaction: AutocompleteInteraction<'cached'>) => Awaitable<readonly ApplicationCommandOptionChoiceData[]>;
+  onButton?: (this: ExtendedClient, interaction: ButtonInteraction<'cached'>, buttonId: string) => Awaitable<void>;
+  onModalSubmit?: (this: ExtendedClient, interaction: ModalSubmitInteraction<'cached'>, modalId: string) => Awaitable<void>;
+  onSelectMenu?: (this: ExtendedClient, interaction: AnySelectMenuInteraction<'cached'>, menuId: string) => Awaitable<void>;
 
   constructor(options: CommandOptions) {
     this.builder = options.builder;
--- src/class/event.ts
+++ src/class/event.ts
@@ -1,18 +1,18 @@
-import type { ClientEvents } from 'discord.js';
+import type { Awaitable, ClientEvents } from 'discord.js';
 import type { ExtendedClient } from '@/class/client';
 
 type Events = keyof ClientEvents;
 
 export interface EventHandlerOptions<Event extends Events = Events> {
   event: Event;
-  on?: (this: ExtendedClient, ...args: ClientEvents[Event]) => void | Promise<void>;
-  once?: (this: ExtendedClient, ...args: ClientEvents[Event]) => void | Promise<void>;
+  on?: (this: ExtendedClient, ...args: ClientEvents[Event]) => Awaitable<void>;
+  once?: (this: ExtendedClient, ...args: ClientEvents[Event]) => Awaitable<void>;
 }
 
 export class EventHandler<Event extends Events = Events> {
   event: Event;
-  on?: (this: ExtendedClient, ...args: ClientEvents[Event]) => void | Promise<void>;
-  once?: (this: ExtendedClient, ...args: ClientEvents[Event]) => void | Promise<void>;
+  on?: (this: ExtendedClient, ...args: ClientEvents[Event]) => Awaitable<void>;
+  once?: (this: ExtendedClient, ...args: ClientEvents[Event]) => Awaitable<void>;
   constructor(options: EventHandlerOptions<Event>) {
     this.event = options.event;
     this.on = options.on;
